909bc188fae199bbb0562ea72adc50975f0479f2
Sigurd <ridderpultost1@gmail.com>
2024-01-22 00:17:53 +0100

    Wrote commment

--- FørsteCraigOppgave/Program.cs
+++ FørsteCraigOppgave/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         Console.WriteLine("Hello, Git!");
         Console.WriteLine("My name is Sigurd");
+        // skriver ut tall en til 12 :)
         int[] time = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         foreach (int num in time)
         {
