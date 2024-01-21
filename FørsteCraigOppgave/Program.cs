39d908da594a969aa9dfab33bafbf4ce361c53a3
Sigurd <ridderpultost1@gmail.com>
2024-01-22 00:17:04 +0100

    Wrote one too 10.

--- FørsteCraigOppgave/Program.cs
+++ FørsteCraigOppgave/Program.cs
@@ -6,6 +6,10 @@ class Program
     {
         Console.WriteLine("Hello, Git!");
         Console.WriteLine("My name is Sigurd");
-        
+        int[] time = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        foreach (int num in time)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
